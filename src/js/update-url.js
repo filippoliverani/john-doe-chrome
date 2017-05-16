db2d6fa938a6234e07c76e52bab4d0e45379764b
Filippo Liverani <filippo-liverani@users.noreply.github.com>
2017-05-16 21:45:44 +0200

    Add more tracking parameters to url cleaning regex

--- src/js/update-url.js
+++ src/js/update-url.js
@@ -1,16 +1,16 @@
-﻿const toBeRemoved = /([\?\&]utm_(([_a-z0-9=]+))=[^&#]+)/ig;
+﻿const toBeRemoved = /([\?\&](utm_|fb_|ref_|action_)([_a-z1-9=]+)|(feature|ref|fref|hc_location|yclid)=[^&#]+)/ig;
 
 function updateUrl(url) {
-  var queryStringIndex = url.indexOf('?');
+  const queryStringIndex = url.indexOf('?');
   if (queryStringIndex < 0) return url;
 
-  var updatedUrl = url.replace(toBeRemoved, '');;
-  if (updatedUrl.indexOf('&') == queryStringIndex) {
+  let updatedUrl = url.replace(toBeRemoved, '');
+  if (updatedUrl.indexOf('&') === queryStringIndex) {
     updatedUrl = updatedUrl.replace('&', '?');
   }
 
   return updatedUrl;
-};
+}
 
 if (typeof module !== 'undefined') {
   module.exports = updateUrl;
