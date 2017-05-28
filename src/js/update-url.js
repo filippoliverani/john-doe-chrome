669c5effb22c47f3270e45edc140c08bcd2dd3c7
Filippo Liverani <filippo-liverani@users.noreply.github.com>
2017-05-28 19:44:32 +0200

    Fix url tracking parameters removal refex, too aggressive

--- src/js/update-url.js
+++ src/js/update-url.js
@@ -1,4 +1,4 @@
-﻿const toBeRemoved = /([\?\&](utm_|fb_|ref_|action_)([_a-z1-9=]+)|(feature|ref|fref|hc_location|yclid)=[^&#]+)/ig;
+﻿const toBeRemoved = /([\?\&](utm_|fb_|ref_)([_a-z1-9=]+)|(action_object_map|action_type_map|action_ref_map|feature|ref|fref|hc_location|yclid)=[^&#]+)/ig;
 
 function updateUrl(url) {
   const queryStringIndex = url.indexOf('?');
